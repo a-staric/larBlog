4abaf94d645a21fcf3d2b8e261b9468209fa270a
a-staric <starichenkoan@yandex.by>
2023-04-18 19:51:19 +0300

    Create first REACT component.

---
+++ resources/js/components/Counter.js
@@ -0,0 +1,31 @@
+// resources/js/components/Counter.js
+
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+export default function Counter() {
+    // Set the initial count state to zero, 0
+    const [count, setCount] = useState(0);
+
+    // Create handleIncrement event handler
+    const handleIncrement = () => {
+        setCount(prevCount => prevCount + 1)
+    };
+
+    // Create handleDecrement event handler
+    const handleDecrement = () => {
+        setCount(prevCount => prevCount - 1);
+    };
+
+    return(
+        <div>
+            <button onClick={handleDecrement}>Minus 1</button>
+            <span> {count} </span>
+            <button onClick={handleIncrement}>Plus 1</button>
+        </div>
+    );
+}
+
+if(document.getElementById('counter')) {
+    ReactDOM.render(<Counter />, document.getElementById('counter'))
+}
